36bef3bb64f5cd4c915d11a9203cb80edc896f4e
Matthew Braun <matt@magnatemobile.com>
2022-10-20 21:16:21 -0400

    Adds TypeScript types.

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+export = remarkAttr;
+declare function remarkAttr(userConfig: any): void;
+declare namespace remarkAttr {
+    export { supportedElements as SUPPORTED_ELEMENTS };
+}
+declare const supportedElements: Set<string>;
